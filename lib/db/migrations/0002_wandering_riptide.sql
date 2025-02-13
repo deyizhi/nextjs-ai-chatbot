02ba7b63ee347cf0fccae887d5069f63bfbec852
deyizhi <hbdl_119@126.com>
2025-02-13 11:15:25 -0800

    fix

--- lib/db/migrations/0002_wandering_riptide.sql
+++ lib/db/migrations/0002_wandering_riptide.sql
@@ -12,24 +12,28 @@ CREATE TABLE IF NOT EXISTS "Vote" (
 	"isUpvoted" boolean NOT NULL,
 	CONSTRAINT "Vote_chatId_messageId_pk" PRIMARY KEY("chatId","messageId")
 );
---> statement-breakpoint
-ALTER TABLE "Chat" ADD COLUMN "title" text NOT NULL;--> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "Message" ADD CONSTRAINT "Message_chatId_Chat_id_fk" FOREIGN KEY ("chatId") REFERENCES "public"."Chat"("id") ON DELETE no action ON UPDATE no action;
+    ALTER TABLE "Chat" ADD COLUMN "title" text NOT NULL;
 EXCEPTION
- WHEN duplicate_object THEN null;
+    WHEN duplicate_object THEN null;
 END $$;
---> statement-breakpoint
+
 DO $$ BEGIN
- ALTER TABLE "Vote" ADD CONSTRAINT "Vote_chatId_Chat_id_fk" FOREIGN KEY ("chatId") REFERENCES "public"."Chat"("id") ON DELETE no action ON UPDATE no action;
+    ALTER TABLE "Message" ADD CONSTRAINT "Message_chatId_Chat_id_fk" FOREIGN KEY ("chatId") REFERENCES "public"."Chat"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
- WHEN duplicate_object THEN null;
+    WHEN duplicate_object THEN null;
 END $$;
---> statement-breakpoint
+
 DO $$ BEGIN
- ALTER TABLE "Vote" ADD CONSTRAINT "Vote_messageId_Message_id_fk" FOREIGN KEY ("messageId") REFERENCES "public"."Message"("id") ON DELETE no action ON UPDATE no action;
+    ALTER TABLE "Vote" ADD CONSTRAINT "Vote_chatId_Chat_id_fk" FOREIGN KEY ("chatId") REFERENCES "public"."Chat"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
- WHEN duplicate_object THEN null;
+    WHEN duplicate_object THEN null;
 END $$;
---> statement-breakpoint
+
+DO $$ BEGIN
+    ALTER TABLE "Vote" ADD CONSTRAINT "Vote_messageId_Message_id_fk" FOREIGN KEY ("messageId") REFERENCES "public"."Message"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+    WHEN duplicate_object THEN null;
+END $$;
+
 ALTER TABLE "Chat" DROP COLUMN IF EXISTS "messages";
