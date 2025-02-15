cccc0acd700f77db55c73f965964de8616957fef
deyizhi <hbdl_119@126.com>
2025-02-15 11:01:12 -0800

    [test online] old mkdown

--- components/memoized-markdown.tsx
+++ components/memoized-markdown.tsx
@@ -9,8 +9,25 @@ import rehypeRaw from 'rehype-raw'
 
 const components: Components = {
     // @ts-expect-error
-    code: CodeBlock,
-    div: ({ children }) => <>{children}</>,
+    code: ({ node, inline, className, children, ...props }) => {
+      const match = /language-(\w+)/.exec(className || '');
+      return !inline && match ? (
+        // @ts-expect-error
+        <pre
+          {...props}
+          className={`${className} text-sm w-[80dvw] md:max-w-[500px] overflow-x-scroll bg-zinc-100 p-3 rounded-lg mt-2 dark:bg-zinc-800`}
+        >
+          <code className={match[1]}>{children}</code>
+        </pre>
+      ) : (
+        <code
+          className={`${className} text-sm bg-zinc-100 dark:bg-zinc-800 py-0.5 px-1 rounded-md`}
+          {...props}
+        >
+          {children}
+        </code>
+      );
+    },
     ol: ({ node, children, ...props }) => {
       return (
         <ol className="list-decimal list-outside ml-4" {...props}>
