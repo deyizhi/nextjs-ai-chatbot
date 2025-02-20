5cf55f525e5bd9d19790919ca5c0751fd22e1f2e
deyizhi <hbdl_119@126.com>
2025-02-20 02:15:35 -0800

    temp del pyodide

--- app/(chat)/layout.tsx
+++ app/(chat)/layout.tsx
@@ -18,10 +18,6 @@ export default async function Layout({
 
   return (
     <>
-      <Script
-        src="https://cdn.jsdelivr.net/pyodide/v0.23.4/full/pyodide.js"
-        strategy="beforeInteractive"
-      />
       <SidebarProvider defaultOpen={!isCollapsed}>
         <AppSidebar user={session?.user} />
         <SidebarInset>{children}</SidebarInset>
